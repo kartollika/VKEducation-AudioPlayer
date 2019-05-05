ba4ecb84b3fc5b8476e1025d199b8aa1db122bab
Dmitrii Maksimov <maksimow.dmitrij@yandex.ru>
2019-05-05 21:46:58 +0300

    Update audio-service branch
    Took 10 minutes

--- app/src/main/java/kartollika/vkeducation/audioplayer/views/main_screen/MainActivity.kt
+++ app/src/main/java/kartollika/vkeducation/audioplayer/views/main_screen/MainActivity.kt
@@ -1,26 +1,38 @@
 package kartollika.vkeducation.audioplayer.views.main_screen
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.support.design.widget.BottomSheetBehavior
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
 import kartollika.vkeducation.audioplayer.R
-import kartollika.vkeducation.audioplayer.views.player_view.FloatingBottomPlayer
-import kotlinx.android.synthetic.main.activity_main.*
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
-import android.os.IBinder
-import android.support.v7.app.AppCompatActivity
 import kartollika.vkeducation.audioplayer.player.PlayerBinder
 import kartollika.vkeducation.audioplayer.player.PlayerService
+import kartollika.vkeducation.audioplayer.views.player_view.FloatingBottomPlayer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainActivityContract.MainActivityView {
 
     private lateinit var presenter: MainActivityPresenter
     private lateinit var floationBottomPlayer: FloatingBottomPlayer
+
+    private var playerService: PlayerService? = null
+    private var isPlayerBounded = false
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isPlayerBounded = false
+        }
+
+        override fun onServiceConnected(name: ComponentName?, binder: IBinder?) {
+            playerService = (binder as PlayerBinder).playerService
+            isPlayerBounded = true
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         presenter = MainActivityPresenter(this)
@@ -42,24 +54,9 @@ class MainActivity : AppCompatActivity(), MainActivityContract.MainActivityView 
             }
         })
 
-
     }
 
     override fun openFolderSelectView() {
-    }
-
-    private var playerService: PlayerService? = null
-    private var isPlayerBounded = false
-
-    private val serviceConnection = object : ServiceConnection {
-        override fun onServiceDisconnected(name: ComponentName?) {
-            isPlayerBounded = false
-        }
-
-        override fun onServiceConnected(name: ComponentName?, binder: IBinder?) {
-            playerService = (binder as PlayerBinder).playerService
-            isPlayerBounded = true
-        }
     }
 
     override fun onStart() {
