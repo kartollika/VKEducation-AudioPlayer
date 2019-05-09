07eae9000614aaa7407eab1bc858ec02b0ad4c06
Dmitrii Maksimov <maksimow.dmitrij@yandex.ru>
2019-05-09 20:58:17 +0300

    Add fade when scrolling to another element in carousel
    Took 48 minutes

--- app/src/main/java/kartollika/vkeducation/audioplayer/common/views/ZoomCentralLayoutManager.kt
+++ app/src/main/java/kartollika/vkeducation/audioplayer/common/views/ZoomCentralLayoutManager.kt
@@ -3,6 +3,7 @@ package kartollika.vkeducation.audioplayer.common.views
 import android.content.Context
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import kotlin.math.sign
 
 
 class ZoomCentralLayoutManager(context: Context?, orientation: Int, reverseLayout: Boolean) :
@@ -10,14 +11,18 @@ class ZoomCentralLayoutManager(context: Context?, orientation: Int, reverseLayou
 
     private val mShrinkAmount = 0.15f
     private val mShrinkDistance = 0.9f
+    private val alphaLowerLimit = 0.5f
 
     override fun scrollHorizontallyBy(
-        dx: Int,
-        recycler: RecyclerView.Recycler?,
-        state: RecyclerView.State?
-    ): Int {
+        dx: Int, recycler: RecyclerView.Recycler?, state: RecyclerView.State?): Int {
         val scrolled = super.scrollHorizontallyBy(dx, recycler, state)
 
+        calculateScale()
+        calculateFade()
+        return scrolled
+    }
+
+    private fun calculateScale() {
         val midpoint = width / 2f
         val d0 = 0f
         val d1 = mShrinkDistance * midpoint
@@ -34,7 +39,30 @@ class ZoomCentralLayoutManager(context: Context?, orientation: Int, reverseLayou
                 view.scaleY = scale
             }
         }
-        return scrolled
+    }
+
+    /** f(X1)-( f(X1) - f(X3) )*(X - X1)/(X2 - X1)
+     * X1 - border to set alpha = f(X1) = 1
+     * X2 = border to set alpha f(X2) = 0.5
+     * X3 - midpoint of view
+     * */
+    private fun calculateFade() {
+        val midpoint = width / 2f
+
+        for (i in 0..childCount) {
+            val view = getChildAt(i)
+            view?.let {
+                val childMidpoint = (getDecoratedRight(view) + getDecoratedLeft(view)) / 2f
+                val sideSign = sign(childMidpoint - midpoint)
+                val x2 = if (sideSign >= 0) {
+                    width
+                } else {
+                    0
+                }
+                view.alpha =
+                    (1f - (1f - 0.5) * (childMidpoint - midpoint + sideSign * 20) / (x2 - midpoint + sideSign * 20)).toFloat()
+            }
+        }
     }
 
     override fun onLayoutChildren(recycler: RecyclerView.Recycler?, state: RecyclerView.State?) {
