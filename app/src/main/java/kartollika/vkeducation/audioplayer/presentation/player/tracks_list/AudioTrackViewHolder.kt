7a7cf861ba795c776156d772a19b66e4783f5029
Dmitrii Maksimov <maksimow.dmitrij@yandex.ru>
2019-05-11 01:27:56 +0300

    Moved artist name and song name out of recycler view
    Took 8 minutes

--- app/src/main/java/kartollika/vkeducation/audioplayer/presentation/player/tracks_list/AudioTrackViewHolder.kt
+++ app/src/main/java/kartollika/vkeducation/audioplayer/presentation/player/tracks_list/AudioTrackViewHolder.kt
@@ -5,19 +5,14 @@ import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
 import kartollika.vkeducation.audioplayer.player.AudioTrack
 import kotlinx.android.synthetic.main.audiotrack_item.view.*
 
 class AudioTrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private var previewImage: ImageView = itemView.albumPreviewImageView
-    private var artistNameTextView: TextView = itemView.artistNameTextView
-    private var songNameTextView: TextView = itemView.songNameTextView
 
     fun bind(audioTrack: AudioTrack) {
-        artistNameTextView.text = audioTrack.artist
-        songNameTextView.text = audioTrack.title
         val albumArt = audioTrack.albumArt
         when (albumArt) {
             is Uri -> previewImage.setImageURI(albumArt)
