a295ff8f6eba91619a262222599a149c2303d06b
Dmitrii Maksimov <maksimow.dmitrij@yandex.ru>
2019-05-13 21:43:30 +0300

    Fix scale interpolation
    Took 1 hour 10 minutes

--- app/src/main/java/kartollika/vkeducation/audioplayer/common/views/AudioTracksCarouselRecyclerView.kt
+++ app/src/main/java/kartollika/vkeducation/audioplayer/common/views/AudioTracksCarouselRecyclerView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
-import android.util.Log
 
 class AudioTracksCarouselRecyclerView(context: Context, attrs: AttributeSet?, defStyle: Int) :
     RecyclerView(context, attrs, defStyle) {
@@ -22,10 +21,6 @@ class AudioTracksCarouselRecyclerView(context: Context, attrs: AttributeSet?, de
                     if (childCount > 0) {
                         val sidePadding = (width / 2) - (getChildAt(0).width / 2)
                         setPadding(sidePadding, 0, sidePadding, 0)
-                        Log.d(
-                            "padding",
-                            "width = $width, childwidth = ${getChildAt(0).width / 2}, sidepadding = $sidePadding )"
-                        )
                     }
                 }
             }
